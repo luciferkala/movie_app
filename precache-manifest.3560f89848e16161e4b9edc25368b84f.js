67c837f1074c8abe31f977df4ca4503ce00bb69f
maestroprog <maestroprog@naver.com>
2018-10-10 08:56:19 +0900

    Updates

---
+++ precache-manifest.3560f89848e16161e4b9edc25368b84f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "bbec8a4b33a11b2aef12",
+    "url": "/movie_app/static/js/main.bbec8a4b.chunk.js"
+  },
+  {
+    "revision": "872633813eb3847f201b",
+    "url": "/movie_app/static/js/1.87263381.chunk.js"
+  },
+  {
+    "revision": "bbec8a4b33a11b2aef12",
+    "url": "/movie_app/static/css/main.0102185e.chunk.css"
+  },
+  {
+    "revision": "edcc7d8dffc73531d6c112f1664cea1b",
+    "url": "/movie_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.9f27855872b27d96e6ba036eb066233e.js"
+  "/movie_app/precache-manifest.3560f89848e16161e4b9edc25368b84f.js"
 );
 
 workbox.clientsClaim();
