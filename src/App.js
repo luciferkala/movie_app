4b37174c924f047ed22752d94a947c84342d4985
maestroprog <maestroprog@naver.com>
2018-10-10 08:30:56 +0900

    state concept

--- src/App.js
+++ src/App.js
@@ -23,18 +23,27 @@ const movies = [
 
 class App extends Component {
 
+  state = {
+    greeting: 'Hello'
+  }
+
   componentWillMount(){
     console.log("will mount");
   }
 
   componentDidMount(){
-    console.log("Did mount");
+    setTimeout(()=> {
+      this.setState({
+        greeting: 'hello again'
+      })
+    },5000)
   }
 
   render() {
     console.log("did render");
     return (
       <div className="App">
+        {this.state.greeting}
         {movies.map((movie,index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}/>
         })
