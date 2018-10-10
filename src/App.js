239d1f38eb6f9d73a49227d270cbfefa0cfdee82
maestroprog <maestroprog@naver.com>
2018-10-11 01:19:20 +0900

    Introducing AJAX
    and fetch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Movie from './Movie';
 class App extends Component {
 
   state = {
+    
   }
 
   componentWillMount(){
@@ -12,33 +13,8 @@ class App extends Component {
   }
 
   componentDidMount(){
-    setTimeout(()=> {
-      this.setState({
-        movies: [
-          {
-            title:   "Venom",
-            poster: "https://extmovie.maxmovie.com/xe/files/attach/images/135/783/904/036/0d87a693a33d087b39c213578dbba00e.jpeg"
-          },
-          {
-            title:   "The Great War",
-            poster: "http://cphoto.asiae.co.kr/listimglink/6/2018051810255135369_1526606750.jpg"
-          },
-          {
-            title:   "Search",
-            poster: "http://viewers.heraldcorp.com/news/photo/201808/17908_12721_585.jpg"
-          },
-          {
-            title:   "Negotiation",
-            poster:  "http://tenasia.hankyung.com/webwp_kr/wp-content/uploads/2018/08/2018082808481766624-540x772.jpg"
-          },
-          {
-            title: "murder",
-            poster: "http://dimg.donga.com/wps/SPORTS/IMAGE/2018/09/06/91858817.1.jpg"
-          }
-        ]
-
-      })
-    },5000)
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+  
   }
 _renderMovies = () => {
   const movies = this.state.movies.map((movie,index) => {
