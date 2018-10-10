8bd1c2c018998fe86964f7392c44dca801cc5bea
maestroprog <maestroprog@naver.com>
2018-10-10 13:50:35 +0900

    Updates

---
+++ precache-manifest.2e52208316cd0fdfa2d238049e137214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "47a85d047549fcac21e0",
+    "url": "/movie_app/static/js/main.47a85d04.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/movie_app/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "47a85d047549fcac21e0",
+    "url": "/movie_app/static/css/main.5cdfe9c0.chunk.css"
+  },
+  {
+    "revision": "7f8fef9c81254205c2b255073d930732",
+    "url": "/movie_app/index.html"
+  }
+];
